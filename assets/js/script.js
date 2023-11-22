56674f52577f2e44810bee398d38d397b6d19309
Ika Yébenes Herce <ikayherce@gmail.com>
2023-11-22 10:44:45 +0000

    edit answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,92 +13,92 @@ const questions = [
   {
     "question": "Question 1",
     "answers": [
-      "Capybara flying", "Capybara dreams of flying like a bird", "Capybara chases birds", "Capybara flies while hungry"
+      "Capybara flying", "Capybara dreams of flying like a bird", "Capybara chases birds"
     ],
     "correct": 1
   },
   {
     "question": "Question 2",
     "answers": [
-      "rat is coding", "rat creates a dating app profile", "rat plays sims on the computer", "rat is a millenial"
+      "rat is coding", "rat creates a dating app profile", "rat plays sims on the computer"
     ],
     "correct": 0
   },
   {
     "question": "Question 3",
     "answers": [
-      "kitten makes potion to rule the world", "kitten tries to make his own eye cream ", "kitten sorcerer makes a potion that will save the planet", "kitten gets too invested in science homework"
+      "kitten makes potion to rule the world", "kitten sorcerer makes a potion that will save the planet", "kitten gets too invested in science homework"
     ],
     "correct": 2
   },
   {
     "question": "Question 4",
     "answers": [
-      "a stained glass window at the rat church", "a stained glass window depicting a holy rat", "a stained glass window depicting a possum with a halo", "a stained glass window depicting the possum messiah"
+      "a stained glass window at the rat church", "a stained glass window depicting a holy rat", "a stained glass window depicting a possum with a halo"
     ],
     "correct": 2
   },
   {
     "question": "Question 5",
     "answers": [
-      "raccoon writer convinces other raccoon that his book is good", "raccoon philosopher discusses his theories", "raccoons discussing politics", "raccoon with glasses psychoanalizes another raccoon."
+     "raccoon philosopher discusses his theories", "raccoons discussing politics", "raccoon with glasses psychoanalizes another raccoon."
     ],
-    "correct": 3
+    "correct": 2
   },
   {
     "question": "Question 6",
     "answers": [
-      "woman is lured by evil neon rabbit in the city of sin", "woman finds comfort in the embrace of a giant fluffy rabbit in the neon lights of a big city.", "woman hugs her cursed boyfriend who was transformed into a neon rabbit", "rabbitman the superhero protects a defenseless young woman"
+      "woman is lured by evil neon rabbit in the city of sin", "woman finds comfort in the embrace of a giant rabbit in the neon lights of a big city.", "rabbitman the superhero protects a defenseless young woman"
     ],
     "correct": 1
   },
   {
     "question": "Question 7",
     "answers": [
-      "the white greyhound with the pearl earring", "portrait of an upper class white greyhound", "the fairest greyhound in the land", "white greyhound feels melancholy"
+      "the white greyhound with the pearl earring", "portrait of an upper class white greyhound", "the fairest greyhound in the land"
     ],
     "correct": 0
   },
   {
     "question": "Question 8",
     "answers": [
-      "praying mantis after eating husband", "praying mantis doing aerobics", "praying mantis dances salsa", "praying mantis having a good day"
+      "praying mantis happy after eating husband", "praying mantis doing aerobics", "praying mantis dances salsa"
     ],
     "correct": 1
   }, 
   {
     "question": "Question 9",
     "answers": [
-      "woman hugging gentle monster", "woman hugging a building that is actually a big gentle animals", "woman hugging new species of pokemon", "woman hugging new pet in the year 3000"
+      "woman hugging a building that is actually a big gentle animal", "woman hugging new species of pokemon", "woman hugging new pet in the year 3000"
     ],
-    "correct": 1
+    "correct": 0
   },
   {
     "question": "Question 10",
     "answers": [
-      "ragdoll mice in a doll house", "puppet show with rats", "a couple of mice living in a barbie house", "a couple of mice enjoy their new pink house"
+      "ragdoll mice in a doll house", "puppet show with rats", "a couple of mice living in a barbie house"
     ],
     "correct": 2
   },
   {
     "question": "Question 11",
     "answers": [
-      "times square but instead of people there are white rabbits", "futuristic Alice in Wonderland", "the apocalypse in which white rabbits are the only ones left in the world", "white rabbit gang takes over New York"
+      "times square but instead of people there are white rabbits", "futuristic Alice in Wonderland", "white rabbit gang takes over New York"
     ],
     "correct": 0
   },
   {
     "question": "Question 12",
     "answers": [
-      "meerkats stand in line to buy lottery tickets", "meerkats are impatient to get cigarretes", "meerkats in a capitalist dystopy", "meerkats stand in line to pay at the meerkat supermarket"
+      "meerkats stand in line to buy lottery tickets", "meerkats are impatient to get cigarretes", "meerkats stand in line to pay at the meerkat supermarket"
     ],
-    "correct": 3
+    "correct": 2
   }, 
 
   {
     "question": "Question 13",
     "answers": [
-      "evil corrupt mayor of otter city", "Benedict Cumberbatch as an otter", " oil painting of an otter dressed as a businessman", "portrait of middle aged upper class otter male"
+      "evil corrupt mayor of otter city", "Benedict Cumberbatch as an otter", " oil painting of an otter dressed as a businessman"
     ],
     "correct": 2
   },
@@ -106,7 +106,7 @@ const questions = [
   {
     "question": "Question 14",
     "answers": [
-      "dragon enfuriated because he doesn't understand javascript", "dragon is too old to understand computers", "dragon steps barefoot on a lego piece", "dragon cursing the long customer service phone queues"
+      "dragon enfuriated because he doesn't understand javascript", "dragon steps barefoot on a lego piece", "dragon cursing the long customer service phone queues"
     ],
     "correct": 0
   },
@@ -114,9 +114,9 @@ const questions = [
   {
     "question": "Question 15",
     "answers": [
-      "chicken is a tourist in Egypt", "sacred chicken in Egypt", "royal teal chicken with ancient egyptian jewels supervising the construction of pyramids", "the rare Egyptian teal chicken, now extinct."
+     "sacred chicken in Egypt", "royal teal chicken with ancient egyptian jewels supervising the construction of pyramids", "the rare Egyptian teal chicken, now extinct."
     ],
-    "correct": 2
+    "correct": 1
   }
 ]
 const quizLength = questions.length;
